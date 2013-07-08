51c33a5da82d7fb7989f302e6fcb2e9df06bfdcc
ana-balica <ana.balica@gmail.com>
2013-07-08 11:17:04 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='absolute images', 
+	  version='1.0', 
+	  py_modules=['markdown'],
+      description='Python Markdown Extension for substituting <img/> relative paths with absolute.',
+      author='Ana Balica', 
+      author_email='ana.balica@gmail.com',
+      url='http://github.com/ana-balica/absolute_images', 
+      classifiers=[
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)'
+      ])
